8eddddc532e3c6cf4b23477730fa172a6b21da4f
Rahul Sagar <rahul@gomilestone.com>
2022-08-05 17:16:29 +0530

    Login Screen updated

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Dashboard from './screens/Dashboard'
+import Login from './screens/Login'
 
 const App = () => {
   return (
     <View>
-      <Dashboard />
+      {/* <Dashboard /> */}
+      <Login/>
     </View>
   )
 }
